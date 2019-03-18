7acd541a9980ae8da80249654a8d0c1acb6ddb5e
Joao C. Monteiro <jcmonteiro92@gmail.com>
2019-03-18 13:33:33 -0300

    add frequency response unit test

--- test/test_PID.cpp
+++ test/test_PID.cpp
@@ -3,6 +3,7 @@
 #include <boost/test/unit_test.hpp>
 #include <iostream>
 #include "PIDController.hpp"
+#include <fstream>
 
 using namespace controller;
 
@@ -89,11 +90,8 @@ void testInputChannels(Controller &controller)
     }
 }
 
-void testStepResponse(Controller &controller, const SettingsPID &settings, double sampling, double time_settling, double max_overshoot, double tol_settling_error)
+linear_system::LinearSystem getDoubleIntegrator(double sampling)
 {
-    Input ref(1);
-    ref.setConstant(1);
-
     linear_system::LinearSystem plant;
     Eigen::VectorXd num(3), den(3);
     num << 0, 0, 1;
@@ -104,10 +102,19 @@ void testStepResponse(Controller &controller, const SettingsPID &settings, doubl
     init_out << 0, 0;
     plant.setInitialOutputDerivatives(init_out);
     plant.discretizeSystem();
+    return plant;
+}
+
+void testStepResponse(Controller &controller, const SettingsPID &settings, double sampling, double time_settling, double max_overshoot, double tol_settling_error)
+{
+    Input ref(1);
+    ref.setConstant(1);
+
+    auto plant = getDoubleIntegrator(sampling);
 
     linear_system::LinearSystem prefilter;
     Eigen::VectorXd num_filter(3), den_filter(3);
-    num_filter << 0, 0, settings.ki;
+    num_filter << 0, pow(settings.getNaturalFrequency(), 2), settings.ki;
     den_filter << settings.kd, settings.kp, settings.ki;
     prefilter.setFilter(num_filter, den_filter);
     prefilter.setSampling(sampling);
@@ -138,7 +145,6 @@ void testStepResponse(Controller &controller, const SettingsPID &settings, doubl
         controller.update(
             linear_system::LinearSystem::getTimeFromSeconds(time),
             prefilter.getOutput(),
-            plant.getOutput(),
             plant.getOutput()
         );
         plant.update(controller.getOutput(), linear_system::LinearSystem::getTimeFromSeconds(time));
@@ -165,6 +171,94 @@ void testStepResponse(Controller &controller, const SettingsPID &settings, doubl
               << "(ref , out) = (" << 1 << " , " << plant.getOutput()[0]/ref[0]
               << ")";
         BOOST_ERROR(error.str());
+    }
+}
+
+void testFrequencyResponse(Controller &controller, const SettingsPID &settings, double sampling, double damp, double cutoff, double farpole)
+{
+    double wn = cutoff / damp;
+    double dt = sampling;
+
+    auto plant = getDoubleIntegrator(sampling);
+
+    linear_system::LinearSystem prefilter;
+    Eigen::VectorXd num_filter(3), den_filter(3);
+    num_filter << 0, pow(settings.getNaturalFrequency(), 2), settings.ki;
+    den_filter << settings.kd, settings.kp, settings.ki;
+    prefilter.setFilter(num_filter, den_filter);
+    prefilter.setSampling(sampling);
+    Eigen::MatrixXd init_out_filter(1, 2);
+    init_out_filter << 0, 0;
+    prefilter.setInitialOutputDerivatives(init_out_filter);
+    prefilter.discretizeSystem();
+
+    linear_system::LinearSystem model_closedloop;
+    Eigen::VectorXd num_model(4), den_model(4);
+    num_model << 0, farpole + 2*damp*wn, wn*wn + farpole*2*damp*wn, farpole*wn*wn;
+    den_model << 1, farpole + 2*damp*wn, wn*wn + farpole*2*damp*wn, farpole*wn*wn;
+    model_closedloop.setFilter(num_model, den_model);
+    model_closedloop.setSampling(sampling);
+    Eigen::MatrixXd init_out_model(1, 3);
+    init_out_model << 0, 0, 0;
+    model_closedloop.setInitialOutputDerivatives(init_out_model);
+    model_closedloop.discretizeSystem();
+    //
+    Eigen::MatrixXd init_in_model(1, 3);
+    init_in_model.setZero();
+
+    Eigen::MatrixXd init_in(1, 2), init_in_filter(1, 2);
+    init_in.setZero();
+    init_in_filter.setZero();
+
+    Input ref(1);
+
+    double input_freq_array[] = {cutoff / 10, cutoff, wn};
+    for (double input_freq : input_freq_array)
+    {
+        double T = 2*M_PI / input_freq;
+        double t_final = 10 * T;
+        double t_transient = t_final - 2*T;
+        double time = 0;
+        plant.setInitialState(init_in);
+        prefilter.setInitialState(init_in_filter);
+        model_closedloop.setInitialState(init_in_model);
+        plant.setInitialTime(time);
+        prefilter.setInitialTime(time);
+        model_closedloop.setInitialTime(time);
+        controller.restart();
+        //
+        double amplitude = 0, max_error = 0;
+        //
+        while (time < t_final)
+        {
+            auto time_sim = linear_system::LinearSystem::getTimeFromSeconds(time);
+            ref[0] = std::sin(input_freq * time);
+            prefilter.update(
+                ref,
+                time_sim
+            );
+            controller.update(
+                time_sim,
+                prefilter.getOutput(),
+                plant.getOutput()
+            );
+            plant.update(controller.getOutput(), time_sim);
+
+            model_closedloop.update(prefilter.getOutput(), time_sim);
+
+            if (time > t_transient)
+            {
+                double amplitude_now = model_closedloop.getOutput()[0];
+                double error = std::abs(amplitude_now - plant.getOutput()[0]);
+                if (error > max_error)
+                    max_error = error;
+                if (amplitude_now > amplitude)
+                    amplitude = amplitude_now;
+            }
+            time += dt;
+        }
+        if (max_error / amplitude > 0.1)
+            BOOST_ERROR("PID did not meet the required frequency response");
     }
 }
 
@@ -210,9 +304,9 @@ BOOST_AUTO_TEST_CASE(test_step_response)
     std::cout << "[TEST] step response" << std::endl;
     //
     PID pid(1);
-    double ts_array[] = {0.1, 1, 10, 100, 1000};
+    double ts_array[] = {0.1, 1, 10, 100, 1000, 10000};
     double overshoot_array[] = {0, 0.01, 0.05, 0.1, 0.2, 0.3, 0.4, 0.5};
-    double tol_ratio_overshoot = 1.1;
+    double tol_ratio_overshoot = 1.06;
     double tol_settling_error = 0.025;
     for (double over : overshoot_array)
     {
@@ -223,6 +317,29 @@ BOOST_AUTO_TEST_CASE(test_step_response)
             pid.configure(settings, sampling);
             //
             testStepResponse(pid, settings, sampling, ts, tol_ratio_overshoot * over, tol_settling_error);
+        }
+    }
+}
+
+BOOST_AUTO_TEST_CASE(test_frequency_response)
+{
+    std::cout << "[TEST] frequency response" << std::endl;
+    //
+    PID pid(1);
+    double cutoff_array[] = {0.01, 0.1, 1, 10, 100, 1000};
+    double damp_array[] = {0.5, 0.6, 0.7, 0.8, 0.9, 1.0};
+    for (double cutoff : cutoff_array)
+    {
+        for (double damp : damp_array)
+        {
+            auto settings = SettingsPID::createFromSpecF(damp, cutoff, 10);
+            double wn = cutoff / damp;
+            double sampling = (2*M_PI/wn) / 1000;
+            auto velocity = SettingsFilter::createSecondOrder(0.7, wn * 200, 1);
+            velocity.sampling_period = sampling;
+            pid.configure(settings, velocity);
+            //
+            testFrequencyResponse(pid, settings, sampling, damp, cutoff, settings.getFarPole());
         }
     }
 }
